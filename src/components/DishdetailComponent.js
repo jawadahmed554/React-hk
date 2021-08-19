90f79efb0ef0908772d9f11edbbb73d6b59c9c67
jawadahmed554 <ahmed.jawad554@gmail.com>
2021-08-19 17:09:43 +0500

    router parameter

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     
@@ -23,7 +25,7 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
             )
         })
         return (
-            <div className='col-12 col-md-5 m-1'>
+            <div >
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
                     {cmnts}
@@ -35,7 +37,6 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
     function RenderDish({dish}){
         if(dish != null){
             return(
-                <div className="col-12  col-md-5  m-1">
                     <Card>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardBody>
@@ -43,7 +44,7 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
                             <CardText>{dish.description}</CardText>
                     </CardBody>
                     </Card>
-                </div>  
+               
             )
         }
         else{
@@ -59,12 +60,26 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
         
         return (
             <div className="container">
-             <div className="row">
-                <RenderDish dish={dish} />
-                <RenderComments comments={dish.comments} />
-             </div>
-             </div>
+            <div className="row">
+                <Breadcrumb>
 
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+            </div>
         );
         }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
-import Menu from './menuComponent';
+import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -37,13 +38,23 @@ class Main extends Component {
           />
       );
     }
+    
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return (
       <div>
         <Header /> 
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
+              <Route exact path='/aboutus' component ={()=> <About leaders={this.state.leaders} />} /> 
               <Redirect to="/home" />
           </Switch>
         <Footer />
